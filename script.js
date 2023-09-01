a1cda41e346acc2b6b2c05af39ca7fcfa534cab0
mrunaljadhav02 <112549948+mrunaljadhav02@users.noreply.github.com>
2023-09-01 22:23:45 +0530

    fourth

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ var w = c.width = window.innerWidth,
 		hh = h / 2,
 		
 		opts = {
-			strings: [ 'HAPPY', 'BIRTHDAY','DAKSHATA!','( VADAPAW )' ],
+			strings: ['HAPPY', 'BIRTHDAY','DAKSHATA!','((VADAPAW))'],
 			charSize: 30,
 			charSpacing: 35,
 			lineHeight: 40,
